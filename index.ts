c6f0dafa81436e3505d52d4c433fbe4f1aa2b6fe
Jerico Contreras <contreras.jerico720@gmail.com>
2025-06-23 18:01:23 +0800

    feat: export FrameworkMain component

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import TheButton from './src/components/TheButton.vue'
 import FrameworkWrapper from './src/views/FrameworkWrapper.vue'
+import FrameworkMain from './src/views/FrameworkMain.vue'
 import {setFrameworkRoutes} from './src/router/routerSetup'
 
 
-export { TheButton, setFrameworkRoutes, FrameworkWrapper }
+export { TheButton, setFrameworkRoutes, FrameworkWrapper, FrameworkMain }
